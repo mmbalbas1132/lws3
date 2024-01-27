5598c694acd09f8c5010334b6b6c47de217a7b2c
mmbalbas <mmbalbasgolf@gmail.com>
2024-01-27 14:21:35 +0100

    Definición y creación de rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -26,3 +15,32 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::group(['middleware' => ['auth:sanctum', 'verified'], 'prefix' => 'dashboard'], function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // CRUD para categorías
+    Route::group(['prefix' => 'category'], function () {
+        Route::get('/', \App\Livewire\Dashboard\Category\Index::class)->name('d-category-index'); // Listado
+        Route::get('/create', \App\Livewire\Dashboard\Category\Save::class)->name('d-category-create'); // Formulario de creación
+        Route::get('/edit/{id}', \App\Livewire\Dashboard\Category\Save::class)->name('d-category-edit'); // Formulario de edición
+    });
+
+    // CRUD para etiquetas
+    Route::group(['prefix' => 'tag'], function () {
+        Route::get('/', \App\Livewire\Dashboard\Tag\Index::class)->name('d-tag-index'); // Listado
+        Route::get('/create', \App\Livewire\Dashboard\Tag\Save::class)->name('d-tag-create'); // Formulario de creación
+        Route::get('/edit/{id}', \App\Livewire\Dashboard\Tag\Save::class)->name('d-tag-edit'); // Formulario de edición
+    });
+
+    // CRUD para publicaciones
+    Route::group(['prefix' => 'post'], function () {
+        Route::get('/', \App\Livewire\Dashboard\Post\Index::class)->name('d-post-index'); // Listado
+        Route::get('/create', \App\Livewire\Dashboard\Post\Save::class)->name('d-post-create'); // Formulario de creación
+        Route::get('/edit/{id}', \App\Livewire\Dashboard\Post\Save::class)->name('d-post-edit'); // Formulario de edición
+    });
+});
+
+

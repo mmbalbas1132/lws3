7711f98f8281a6eccc2d78e0428c644d462620c9
mmbalbas <mmbalbasgolf@gmail.com>
2024-01-27 18:10:39 +0100

    Componente para crear categoria y referencias a direcitivas wire-x

--- app/Livewire/Dashboard/Category/Save.php
+++ app/Livewire/Dashboard/Category/Save.php
@@ -2,12 +2,42 @@
 
 namespace App\Livewire\Dashboard\Category;
 
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class Save extends Component
 {
+    public $title;
+    public $text;
     public function render()
     {
-        return view('livewire.dashboard.category.save');
+        return view('livewire.dashboard.category.save')->layout('layouts.app');
+    }
+
+
+    public function submit()
+    {
+        // Validación de los campos con mensajes personalizados
+        $this->validate([
+            'title' => 'required|min:3', // Ejemplo: título requerido y mínimo de 3 caracteres
+            'text' => 'required|min:10', // Ejemplo: texto requerido y mínimo de 10 caracteres
+        ], [
+            'title.required' => 'El título no puede estar vacío.',
+            'title.min' => 'El título debe tener al menos 3 caracteres.',
+            'text.required' => 'El texto no puede estar vacío.',
+            'text.min' => 'El texto debe tener al menos 10 caracteres.',
+        ]);
+
+        // Creación de la categoría
+        Category::create([
+            'title' => $this->title,
+            'slug' => Str::slug($this->title),
+            'text' => $this->text,
+        ]);
+
+        // Restablecer los campos
+        $this->title = '';
+        $this->text = '';
     }
 }
--- resources/views/livewire/dashboard/category/save.blade.php
+++ resources/views/livewire/dashboard/category/save.blade.php
@@ -1,3 +1,24 @@
 <div>
-    {{-- The whole world belongs to you. --}}
+    <form wire:submit.prevent="submit">
+        {{--El comportamiento que estás experimentando, donde la variable no se actualiza hasta que se pulsa el botón de enviar, se debe al uso de wire:submit.prevent="submit" en tu formulario. Cuando se utiliza wire:submit.prevent, Livewire bloquea la acción predeterminada del formulario y realiza una solicitud HTTP para procesar los datos antes de volver a renderizar la página. Esto significa que los cambios en los campos del formulario no se reflejarán en tiempo real en la vista hasta que se envíe el formulario.--}}
+{{--   Consultas:     https://laravel-livewire.com/docs/2.x/properties#binding-inputs-to-properties--}}
+
+        <!-- Campo de título -->
+
+        <h3 class="text-xl">***{{$title}}***</h3>
+        <label for="">Título</label>
+        <input type="text" wire:model.live.debounce="title"><br>
+
+        @error('title') <span class="error bg-red-600 text-white">{{ $message }}</span> @enderror
+        <br>
+
+        <!-- Campo de texto -->
+        <label for="">Texto</label>
+        <input type="text" wire:model="text"><br>
+        <h3 class="text-xl">***{{$text}}***</h3>
+        @error('text') <span class="error bg-red-600 text-white">{{ $message }}</span> @enderror
+        <br>
+        <!-- Botón de enviar -->
+        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">Enviar</button>
+    </form>
 </div>

f4d5873e838a880530eed5b74f72bbee530d3da3
mmbalbas <mmbalbasgolf@gmail.com>
2024-01-27 14:11:22 +0100

    Creación de componentes de Livewire Category/Save Category/Index, Post/Save Post/Index y Tag/Save Tag/Index

---
+++ app/Livewire/Dashboard/Category/Save.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard\Category;
+
+use Livewire\Component;
+
+class Save extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard.category.save');
+    }
+}

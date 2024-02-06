4087513b3d1346d47d7801d3b3140b05457cdf74
mmbalbas <mmbalbasgolf@gmail.com>
2024-02-06 11:38:55 +0100

    Ciclo de vida de un componete 1

--- app/Livewire/Dashboard/Category/Save.php
+++ app/Livewire/Dashboard/Category/Save.php
@@ -6,10 +6,12 @@ use App\Models\Category;
 use Illuminate\Support\Str;
 use Livewire\Component;
 use Livewire\Features\SupportFileUploads\WithFileUploads;
+use Log;
 
 class Save extends Component
 {
     use WithFileUploads;
+//    En este caso no es necesario porque no se cargan archivos en este componente
 
     public $title;
     public $text;
@@ -39,4 +41,49 @@ class Save extends Component
         // Restablecer los campos
         $this->reset(['title', 'text']);
     }
+    public function boot()
+    {
+        Log::info('boot');
+    }
+    public function booted()
+    {
+        Log::info('booted');
+    }
+    public function mount()
+    {
+        Log::info('mount');
+    }
+    public function hydrateTitle($value)
+    {
+        Log::info('hydrateTitle');
+    }
+  public function dehydrateFoo ($value)
+  {
+      Log::info('dehydrateFoo $value');
+  }
+  public function hydrate()
+  {
+      Log::info('hydrate');
+  }
+    public function dehydrate()
+    {
+        Log::info('dehydrate');
+    }
+    public function updating($name, $value)
+    {
+        Log::info('updating $name $value');
+    }
+    public function updated($name, $value)
+    {
+        Log::info('updated $name $value');
+    }
+    public function updatingTitle($value)
+    {
+        Log::info('updatingTitle $value');
+    }
+    public function updatedTitle($value)
+    {
+        Log::info('updatedTitle $value');
+    }
 }
+

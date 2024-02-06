87628bcf3626ed95e97ab85b111f00db0e059e95
mmbalbas <mmbalbasgolf@gmail.com>
2024-02-06 20:07:17 +0100

    Listado de registros paginado

--- app/Livewire/Dashboard/Category/Index.php
+++ app/Livewire/Dashboard/Category/Index.php
@@ -4,14 +4,17 @@ namespace App\Livewire\Dashboard\Category;
 
 use App\Models\Category;
 use Livewire\Component;
+use Livewire\WithPagination; // Importa el trait WithPagination
 
 class Index extends Component
 {
-    public $categories;
+    use WithPagination; // Usa el trait WithPagination
+
+
+
     public function render()
     {
-        $this->categories = Category::all();
-        return view('livewire.dashboard.category.index')->layout('layouts.app');
-//        Tuve que añadir ->layout('layouts.app'); porque si no me daba un error de que no encontraba el layout devido que la estructura de carpetas ha cambiado por la vesión de laravel 10 respecto a la 8   .
+        $categories = Category::paginate(5); // Obtiene los datos paginados
+        return view('livewire.dashboard.category.index', compact('categories'))->layout('layouts.app');
     }
 }
--- resources/views/livewire/dashboard/category/index.blade.php
+++ resources/views/livewire/dashboard/category/index.blade.php
@@ -3,7 +3,7 @@
 <table class="table w-full">
     <thead>
         <tr>
-{{--            <th class="px-4 py-2">ID</th>--}}
+
             <th class="px-4 py-2">Título</th>
             <th class="px-4 py-2">Acciones</th>
         </tr>
@@ -12,20 +12,17 @@
         @foreach($categories as $category)
         <tr>
             <td class="border px-4 py-2">{{ $category->id .' ' . $category->title }}</td>
-{{--            <td class="border px-4 py-2">{{ $category->title }}</td>--}}
+
             <td class="border px-4 py-2">
                 <a href="{{ route('d-category-edit', $category) }}" class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">Editar</a>
                 <x-danger-button>
                     Eliminar
                 </x-danger-button>
-{{--                <form action="{{ route('category.destroy', $category) }}" method="POST" class="inline">--}}
-{{--                    @csrf--}}
-{{--                    @method('DELETE')--}}
-{{--                    <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Eliminar</button>--}}
-{{--                </form>--}}
+
             </td>
         </tr>
         @endforeach
     </tbody>
 </table>
+    {{ $categories->links() }}
 </div>

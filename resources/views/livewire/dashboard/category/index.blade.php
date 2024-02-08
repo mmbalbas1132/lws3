eeb36b99f1edd4c35ab1efb057feb1b69d678e59
mmbalbas <mmbalbasgolf@gmail.com>
2024-02-08 11:49:04 +0100

    Dar estilos al cuerpo y cración de componente de blade card

--- resources/views/livewire/dashboard/category/index.blade.php
+++ resources/views/livewire/dashboard/category/index.blade.php
@@ -1,41 +1,59 @@
-<div>
-    <x-action-message on="deleted" class="text-lg text-white bg-red-500 p-4 rounded-md">
-         <strong>{{__('Deleted category success')}}</strong>
-    </x-action-message>
-<h1>Listado</h1>
-<table class="table w-full">
-    <thead>
-        <tr>
+<div class="p-4 sm:p-8 lg:p-12"> <!-- Ajustar el padding en diferentes tamaños de pantalla -->
+    <x-card>
+        @slot('title')
+            <!-- Mensaje de acción (responsivo) -->
+            <x-action-message on="deleted" class="text-lg text-white bg-red-500 p-4 rounded-md mb-4">
+                <strong>{{__('Deleted category success')}}</strong>
+            </x-action-message>
 
-            <th class="px-4 py-2">Título</th>
-            <th class="px-4 py-2">Acciones</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($categories as $category)
-        <tr>
-            <td class="border px-4 py-2">{{ $category->id .' ' . $category->title }}</td>
-            <td class="border px-4 py-2">
-                @if ($category->image)
-                    <img src="{{ $category->getImageUrlAttribute() }}" alt="{{ $category->title }}" class="w-40 my-3">
+            <!-- Título (responsivo) -->
+            <h1 class="text-3xl sm:text-4xl lg:text-5xl font-bold mb-4">{{__('Listing')}}</h1>
+        @endslot
 
+        <!-- Tabla (responsivo) -->
+        <div class="overflow-x-auto">
+            <table class="w-full table-auto border text-center">
+                <!-- Encabezado de la tabla -->
+                <thead class="text-left bg-gray-100">
+                <tr class="border-b bg-green-300 text-xl p-10 text-center">
+                    <th class="font-bold py-2">{{__('Id')}}</th>
+                    <th class="font-bold py-2">{{__('Title')}}</th>
+                    <th class="font-bold py-2">{{__('Image')}}</th>
+                    <th class="font-bold py-2">{{__('Text')}}</th>
+                    <th class="font-bold py-2">{{__('Actions')}}</th>
+                </tr>
+                </thead>
+                <!-- Cuerpo de la tabla -->
+                <tbody>
+                @foreach($categories as $category)
+                    <tr class="border-b">
+                        <td class="p-2">{{ $category->id }}</td>
+                        <td class="p-2">{{ $category->title }}</td>
+                        <td class="p-2">
+                            @if ($category->image)
+                                <img src="{{ $category->getImageUrlAttribute() }}" alt="{{ $category->title }}" class="w-20 sm:w-32 lg:w-40 my-3 mx-auto">
+                                <br>
+                                <span class="text-sm sm:text-base lg:text-lg">{{ $category->image }}</span> <!-- Mostrar el nombre de la imagen -->
+                            @else
+                                <span class="text-red-600">{{__('No image available')}}</span>
+                            @endif
+                        </td>
+                        <td class="p-2">{{ $category->text }}</td>
+                        <td class="p-2">
+                            <a href="{{ route('d-category-edit', $category) }}" class="'inline-flex items-center justify-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150'">{{__('Edit')}}</a>
+                            <x-danger-button wire:click="delete({{ $category->id }})" onclick="return confirm('¿Seguro que deseas eliminar el registro seleccionado?') || event.stopImmediatePropagation()">
+                                {{__('Remove')}}
+                            </x-danger-button>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </x-card>
 
-                @else
-                    Sin imagen
-                @endif
-            </td>
-
-            <td class="border px-4 py-2">
-                <a href="{{ route('d-category-edit', $category) }}" class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">Editar</a>
-                <x-danger-button wire:click="delete({{ $category->id }})" onclick="return confirm('¿Seguro que deseas eliminar el registro seleccionado?') || event.stopImmediatePropagation()">
-                    Eliminar
-                </x-danger-button>
-
-
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
-    {{ $categories->links() }}
+                                 <!-- Paginación (responsivo) -->
+    <div class="mt-4">
+        {{ $categories->links() }}
+    </div>
 </div>
--- resources/views/livewire/dashboard/category/save.blade.php
+++ resources/views/livewire/dashboard/category/save.blade.php
@@ -1,46 +1,59 @@
-<div>
-    <x-action-message on="created" class="text-lg text-white bg-green-500 p-4 rounded-md">
+<x-card>
+    @slot('title')
+        <h1 class="text-3xl sm:text-4xl lg:text-5xl font-bold mb-4">Crear o actualizar categoría</h1>
+    @endslot
+<div class="p-20">
+    <x-action-message on="created" >
        <strong>{{__('Created category success')}}</strong>
     </x-action-message>
-    <x-action-message on="updated" class="text-lg text-white bg-green-500 p-4 rounded-md">
+    <x-action-message on="updated">
          <strong>{{__('Updated category success')}}</strong>
     </x-action-message>
-    <form wire:submit.prevent="submit">
-        {{--El comportamiento que estás experimentando, donde la variable no se actualiza hasta que se pulsa el botón de enviar, se debe al uso de wire:submit.prevent="submit" en tu formulario. Cuando se utiliza wire:submit.prevent, Livewire bloquea la acción predeterminada del formulario y realiza una solicitud HTTP para procesar los datos antes de volver a renderizar la página. Esto significa que los cambios en los campos del formulario no se reflejarán en tiempo real en la vista hasta que se envíe el formulario.--}}
-{{--   Consultas:     https://laravel-livewire.com/docs/2.x/properties#binding-inputs-to-properties--}}
+    <x-form-section submit="submit">
 
+@slot('title')
+    {{__('Categories')}}
+@endslot
+        @slot('description')
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fugiat possimus tenetur aliquid tempora omnis magni. Obcaecati maiores eveniet veritatis, ullam nemo dolorem sint placeat molestias, et animi numquam ducimus?
+        @endslot
+    @slot('form')
         <!-- Campo de título -->
+          <div class="col-span-6 sm:col-span-4">
+
         <x-label for="">{{__('Title')}}</x-label>
-        @error('title') <span class="error bg-red-600 text-white">{{ $message }}</span> @enderror
-        <x-input type="text" wire:model="title" />
+              <x-input-error for="title"/>
+{{--        @error('title'){{ $message }}@enderror--}}
+        <x-input type="text" wire:model="title" class="block w-full"/>
+          </div>
 
         <!-- Campo de texto -->
+        <div class="col-span-6 sm:col-span-4">
         <x-label for="">{{__('Text')}}</x-label>
-        <x-input type="text" wire:model="text"/>
+        <x-input-error for="text"/>
+        <x-input type="text" wire:model="text" class="block w-full"/>
+        </div>
 
         <!-- Campo de imagen -->
+        <div class="col-span-6 sm:col-span-4">
         <x-label for="">{{__('Image')}}</x-label>
-        <x-input-error for="image" class="mb-4" />
+        <x-input-error for="image"/>
         <x-input type="file" wire:model="image"/>
         @if ($image)
             <img src="{{ $image->temporaryUrl() }}" alt="image" class="w-40 my-3">
         @endif
-
-
+        </div>
+    @endslot
 {{--        Este fragmento de código lo utiliza Livewire para manejar la carga de imágenes. Aquí está el desglose:--}}
-
 {{--        <x-label for="">{{__('Image')}}</x-label>: Esto crea una etiqueta de texto para la imagen. La función {{__('Image')}} se utiliza para traducir la palabra "Image" según el idioma configurado en la aplicación.--}}
-
 {{--        <x-input-error for="image" class="mb-4" />: Esto genera un mensaje de error para el campo de imagen, en caso de que ocurra algún error durante la carga.--}}
-
 {{--        <x-input type="file" wire:model="image"/>: Este es un elemento de entrada de tipo archivo que permite al usuario seleccionar un archivo de su dispositivo local. El atributo wire:model="image" está vinculado a una propiedad de Livewire llamada "image", lo que significa que cuando se selecciona un archivo, se asignará automáticamente a esa propiedad en el componente de Livewire.--}}
-
 {{--        @if ($image): Esto verifica si hay un archivo de imagen seleccionado. Si $image no es nulo, significa que se ha seleccionado un archivo de imagen.--}}
-
 {{--        <img src="{{ $image->temporaryUrl() }}" alt="image" class="w-40 my-3">: Si se ha seleccionado un archivo de imagen, se muestra una etiqueta <img> que muestra una vista previa de la imagen seleccionada. La función temporaryUrl() genera una URL temporal para acceder al archivo de imagen cargado temporalmente. Esto es útil para mostrar una vista previa de la imagen antes de que se guarde definitivamente en el servidor. El atributo alt proporciona un texto alternativo para la imagen, y class="w-40 my-3" se utiliza para aplicar estilos CSS a la imagen (en este caso, un ancho de 40 unidades y un margen superior e inferior de 3 unidades).--}}
-
-
         <!-- Botón de enviar -->
-        <x-button type="submit" class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">{{__('Send')}}</x-button>
-    </form>
+    @slot('actions')
+        <x-button type="submit" >{{__('Send')}}</x-button>
+    @endslot
+    </x-form-section>
 </div>
+</x-card>
